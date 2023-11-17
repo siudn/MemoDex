4533d3ae10491ac6276964e518ce24f76d7419be
Shadman Uddin <cshadx@gmail.com>
2023-11-17 17:01:16 -0500

    Working on sound component

--- src/components/PlaySound.jsx
+++ src/components/PlaySound.jsx
@@ -1,1 +1,32 @@
-function PlaySound(sound) {}
+import { useState } from "react";
+import useSound from "use-sound";
+
+function PlaySound() {
+  const [playing, setPlaying] = useState(true);
+
+  const soundUrl = "../assets/ecruteak.mp3";
+
+  const [play, { stop }] = useSound(soundUrl, { volume: 0.5 });
+
+  const clickHandler = (play) => {
+    if (play) {
+      setPlaying(false);
+      stop();
+    } else {
+      setPlaying(true);
+      play();
+    }
+  };
+
+  return (
+    <div>
+      {() => {
+        return playing ? (
+          <img src="../assets/soundOn.png" onClick={clickHandler(playing)} />
+        ) : (
+          <img src="../assets/soundOff.png" onClick={clickHandler(playing)} />
+        );
+      }}
+    </div>
+  );
+}
