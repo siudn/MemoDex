78f9646c2026d0486bf43d870f5fa9a287b2f6be
thuyencode <thuyencode@proton.me>
2023-11-25 20:22:05 +0700

    'setClickedCards' is never used

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,56 @@
-import { useState, useEffect } from "react";
-import "./App.css";
-import Display from "./components/Display";
-import { shuffle, idGen } from "./components/Random";
-import Scoreboard from "./components/Scoreboard";
-import { Instruction } from "./components/Scoreboard";
-import PlaySound from "./components/PlaySound";
+import { useState, useEffect } from 'react'
+import './App.css'
+import Display from './components/Display'
+import { shuffle, idGen } from './components/Random'
+import Scoreboard from './components/Scoreboard'
+import { Instruction } from './components/Scoreboard'
+import PlaySound from './components/PlaySound'
 
 function App() {
-  const [score, setScore] = useState(0);
-  const [bestScore, setBestScore] = useState(0);
-  const [pokeArr, setPokeArr] = useState(idGen());
-  const [clickedCards, setClickedCards] = useState([]);
+  const [score, setScore] = useState(0)
+  const [bestScore, setBestScore] = useState(0)
+  const [pokeArr, setPokeArr] = useState(idGen())
+  const [clickedCards] = useState([])
 
   const handleIncreaseScore = () => {
-    setScore((prevScore) => prevScore + 1);
-  };
+    setScore((prevScore) => prevScore + 1)
+  }
 
   const reset = () => {
-    setBestScore(score);
-    setScore(0);
-    setPokeArr(idGen());
-  };
+    setBestScore(score)
+    setScore(0)
+    setPokeArr(idGen())
+  }
 
   const clickHandler = (e) => {
     if (!clickedCards.includes(e.target)) {
-      clickedCards.push(e.target);
-      handleIncreaseScore();
-      const shuffledIDs = [...pokeArr];
-      const newArr = shuffle(shuffledIDs);
-      setPokeArr(newArr);
+      clickedCards.push(e.target)
+      handleIncreaseScore()
+      const shuffledIDs = [...pokeArr]
+      const newArr = shuffle(shuffledIDs)
+      setPokeArr(newArr)
     } else {
-      reset();
+      reset()
     }
-  };
+  }
 
   useEffect(() => {
     if (score == 10) {
-      reset();
-      alert("You Win!");
+      reset()
+      alert('You Win!')
     }
-  }, [score]);
+  }, [score])
 
   return (
     <div /*className="h-full flex flex-col justify-center align-middle"*/>
       <PlaySound></PlaySound>
-      <div className="flex flex-col gap-8">
+      <div className='flex flex-col gap-8'>
         <Instruction></Instruction>
         <Display IDs={pokeArr} clickHandle={clickHandler}></Display>
         <Scoreboard score={score} bestScore={bestScore}></Scoreboard>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
