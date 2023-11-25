8a3282dda3757d20e011b428a82c440f8068d725
thuyencode <thuyencode@proton.me>
2023-11-25 20:22:33 +0700

    Remove unused imports

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,14 +1,13 @@
-import { useEffect, useState } from "react";
-import Card from "./Card";
+import Card from './Card'
 
 function Display({ clickHandle, IDs }) {
   return (
-    <div className="grid grid-cols-5 gap-4">
+    <div className='grid grid-cols-5 gap-4'>
       {IDs.map((x) => (
         <Card key={x} name={x} handleClick={clickHandle}></Card>
       ))}
     </div>
-  );
+  )
 }
 
-export default Display;
+export default Display
