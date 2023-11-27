4a774453f125a7fc459d5714f2d660195bf5251f
thuyencode <thuyencode@proton.me>
2023-11-27 10:18:49 +0700

    mockServer config

--- setup-tests/setupTests.js
+++ setup-tests/setupTests.js
@@ -1,9 +1,19 @@
 import * as matchers from '@testing-library/jest-dom/matchers'
 import { cleanup } from '@testing-library/react'
-import { afterEach, expect } from 'vitest'
+import { afterAll, afterEach, beforeAll, expect } from 'vitest'
+import mockServer from './mockServer'
 
 expect.extend(matchers)
 
 afterEach(() => {
   cleanup()
+  mockServer.resetHandlers()
+})
+
+beforeAll(() => {
+  mockServer.listen({ onUnhandledRequest: 'error' })
+})
+
+afterAll(() => {
+  mockServer.close()
 })
