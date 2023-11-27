1547102a9218f02b956b4dda2934567f489e1b01
thuyencode <thuyencode@proton.me>
2023-11-27 14:59:47 +0700

    Rename example db file

--- tests/Card.test.jsx
+++ tests/Card.test.jsx
@@ -3,20 +3,20 @@ import { HttpResponse, http } from 'msw'
 import { expect, test } from 'vitest'
 import mockServer from '../setup-tests/mockServer'
 import Card from '../src/components/Card'
-import voltorb from './example-db/voltorb.json'
+import pokeID100 from './example-db/100.json'
 
 test("Should display Pokemon's name and image", async () => {
   const EXAMPLE_POKE_ID = 100
 
   mockServer.use(
     http.get(`https://pokeapi.co/api/v2/pokemon/${EXAMPLE_POKE_ID}`, () => {
-      return HttpResponse.json(voltorb)
+      return HttpResponse.json(pokeID100)
     })
   )
   render(<Card id={EXAMPLE_POKE_ID} handleClick={() => {}} />)
 
-  expect(await screen.findByText('voltorb')).toBeInTheDocument()
+  expect(await screen.findByText(pokeID100.name)).toBeInTheDocument()
   expect(
-    await screen.findByRole('img', { name: voltorb.name })
+    await screen.findByRole('img', { name: pokeID100.name })
   ).toBeInTheDocument()
 })
